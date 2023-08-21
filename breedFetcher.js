5566238a40416f4108a551b54c16f566031257c3
Chandani Kumari <chandaanikumari92@gmail.com>
2023-08-21 00:08:54 -0700

    implementing fetchbreed function

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const URL = `https://api.thecatapi.com/v1/breeds/search?q=sib`;
+console.log("Fetching data...");
+const fetchBreed = function(breed, callback) {
+  request(URL, (err, response, body) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      let data = JSON.parse(body);
+      console.log(data['description']);
+      console.log(typeof data);
+      const breedData = data[0];
+      if (breedData) {
+        callback(null, breed.description);
+      }
+      callback(`${breed} information is not not available`, null);
+    }
+  });
+};
+
